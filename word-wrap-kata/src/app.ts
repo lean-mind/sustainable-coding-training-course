b3ff042f7ae27b0437f95da384199d5a45853497
Miguel Ignacio Cabrera San Gil <miguel.cabrera@leanmind.es>
2025-01-16 18:18:26 +0000

    chore: format all files

--- word-wrap-kata/src/app.ts
+++ word-wrap-kata/src/app.ts
@@ -1,12 +1,12 @@
-import {Controller} from "./controller";
+import { Controller } from './controller'
 
 export class App {
   public initialize(aDocument: Document): void {
     const controller = new Controller()
-    controller.subscribeToEvents(
-        aDocument, {
-          columnWidthId:'column-width',
-          editorId: 'editor',
-          applyId: 'do-word-wrap'})
+    controller.subscribeToEvents(aDocument, {
+      columnWidthId: 'column-width',
+      editorId: 'editor',
+      applyId: 'do-word-wrap',
+    })
   }
 }
